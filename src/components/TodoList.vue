26434dd477dfa2ac166557fe88849df23a4b4945
kacelab <jung@kacelab.com>
2022-05-11 13:28:49 +0900

    Initial commit

---
+++ src/components/TodoList.vue
@@ -0,0 +1,34 @@
+<template>
+  <section>
+    <ul>
+      <li v-for="todoItem in todoItems" v-bind:key="todoItem"> {{todoItem}} </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "TodoList",
+  data(){
+    return {
+      todoItems: []
+    }
+  },
+
+  created() {
+    if(localStorage.length > 0){
+      for (var i=0; i<localStorage.length; i++){
+        this.todoItems.push(localStorage.key(i));
+      }
+
+    }
+
+
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
