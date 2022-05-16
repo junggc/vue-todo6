df6bad5b07e1a17178b2d5b095488874b015c0a5
kacelab <jung@kacelab.com>
2022-05-16 19:17:22 +0900

    gg

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,7 @@
 <template>
   <section>
     <ul>
-      <li v-for="(todoItem, index) in todoItems"  :key="todoItem" class="shadow">
+      <li v-for="(todoItem, index) in propsdata"  :key="todoItem" class="shadow">
         <i class="checkBtn fa fa-check" aria-hidden="true"></i>
         {{ todoItem }}
         <span class="removeBtn" type="button" @click="removeTodo(todoItem, index)">
@@ -15,28 +15,14 @@
 <script>
 export default {
   name: "TodoList",
-  data(){
-    return {
-      todoItems: []
-    }
-  },
+  props:['propsdata'],
 
   methods:{
     removeTodo(todoItem, index){
-     localStorage.removeItem(todoItem);
-     this.todoItems.splice(index,1);
+    this.$emit('removeTodo',todoItem,index);
     }
-  },
-  created() {
-    if(localStorage.length > 0){
-      for (var i=0; i<localStorage.length; i++){
-        this.todoItems.push(localStorage.key(i));
-      }
-
-    }
-
-
   }
+
 
 }
 </script>
